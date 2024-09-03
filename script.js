9e993491e072f8e43a3fca56d5773ed8f7613f3d
Faith Amarachi M. <oluebube.madu@yahoo.com>
2024-09-03 17:46:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,171 @@
-// getting all required elements
-// getting circle elements
-let firstCircle = document.querySelector(".first");
-let secondCircle = document.querySelector(".second");
-let thirdCircle = document.querySelector(".third");
-let forthCircle = document.querySelector(".forth");
-let firstCircleMobile = document.querySelector(".first-mobile");
-let secondCircleMobile = document.querySelector(".second-mobile");
-let thirdCircleMobile = document.querySelector(".third-mobile");
-let forthCircleMobile = document.querySelector(".forth-mobile");
+const steps = document.querySelectorAll(".stp");
+const circleSteps = document.querySelectorAll(".step");
+const formInputs = document.querySelectorAll(".step-1 form input");
+const plans = document.querySelectorAll(".plan-card");
+const  switcher = document.querySelector(".switch");
+const total = document.querySelector(".total b");
+const addons = document.querySelectorAll(".box");
+const planPrice = document.querySelector(".plan-price");
+let time;
+let currentStep = 1;
+let currentCircle = 0;
+const obj = {
+    plan: null,
+    kind: null,
+    price: null,
+};
 
-// getting details elements for different pages
-let detailsInfo = document.querySelector(".info");
-let detailsPlan = document.querySelector(".plan");
-let detailsAddon = document.querySelector(".add-ons");
-let detailsSummary = document.querySelector(".summary");
+steps.forEach((step) => {
+    const nextBtn = step.querySelector(".next-stp");
+    const prevBtn = step.querySelector(".prev-stp");
+    if (prevBtn){
+        prevBtn.addEventListener("click", () =>{
+            document.querySelector(`.step-${currentStep}`).style.display = "none";
+            currentStep--;
+            document.querySelector(`.step-${currentStep}`).style.display = "flex";
+            circleSteps[currentCircle].classList.remove("active");
+            currentCircle--;
+        })
+    }
+    nextBtn.addEventListener("click", () =>{
+        document.querySelector(`.step-${currentStep}`).style.display = "none";
+        if (currentStep < 5 && validateForm()){
+            currentStep++;
+            currentCircle++;
+            setTotal();
+        }
+        document.querySelector(`.step-${currentStep}`).style.display = "flex";
+        circleSteps[currentCircle].classList.add("active");
+        summary(obj);
+    })
+})
 
-// btn elements
-let backBtn = document.querySelector(".back-btn");
-let nextBtn = document.querySelector(".details-btn");
+function summary(obj){
+    const planName = document.querySelector(".plan-name");
+    const planPrice = document.querySelector(".plan-price");
+    planPrice.innerHTML = `${obj.price.innerText}`;
+    planName.innerHTML = `${obj.plan.innerText} (${obj.kind ? "yearly" : "monthly"})`;
+}
+function validateForm(){
+    let valid = true;
+    for (let i = 0; i < formInputs.length; i++){
+        if (!formInputs[i].value){
+            valid = false;
+            formInputs[i].classList.add('err');
+            findLabel(formInputs[i]).nextElementSibling.style.display = "flex";
+            console.log("error");
+        } else{
+            valid = true;
+            formInputs[i].classList.remove("err");
+            findLabel(formInputs[i]).nextElementSibling.style.display = "none";
+        }
+    }
+    return valid;
+}
+function findLabel(el){
+    const idVal = el.id;
+    const labels = document.getElementsByTagName("label");
+    for (let i = 0; i < labels.length; i++){
+        if (labels[i].htmlFor == idVal) return labels[i];
+    }
+}
 
-// click function for first circle
-firstCircle.onclick = () => {
-    detailsSummary.style.display = "none";
-    detailsPlan.style.display = "none";
-    detailsAddon.style.display = "none";
-    detailsInfo.style.display = "block";
-    backBtn.style.display = "none";
+plans.forEach((plan) => {
+    plan.addEventListener("click", () => {
+        document.querySelector(".selected").classList.remove("selected");
+        plan.classList.add("selected");
+        const planName = plan.querySelector("b");
+        const planPrice = plan.querySelector(".plan-priced");
+        obj.plan = planName;
+        obj.price = planPrice;
+    })
+})
+
+
+switcher.addEventListener("click", () =>{
+    const val = switcher.querySelector("input").checked;
+    if (val){
+        document.querySelector(".monthly").classList.remove("sw-active");
+        document.querySelector(".yealry").classList.add("sw-active");
+    }else {
+        document.querySelector(".montly").classList.add("sw-active")
+        document.querySelector(".yearly").classList.remove("sw-active");
+    }
+    switchPrice(val);
+    obj.kind = val;
+})
+
+
+addons.forEach((addon) => {
+    addon.addEventListener("click", (e) =>{
+        const addonSelect = addon.querySelector("input");
+        const ID = addon.getAttribute("data-id");
+        if (addonSelect.checked){
+            addonSelect.checked = false;
+            addon.classList.remove("ad-selected");
+            showAddon(ID, false);
+        }else {
+            addonSelect.checked = true;
+            addon.classList.add("add-selected");
+            showAddon(addon, true);
+            e.preventDefault()
+        }
+    })
+})
+
+
+function switchPrice(checked){
+    const yearlyPrice = [90, 120, 150];
+    const monthlyPrice = [9, 12, 15];
+    const prices = document.querySelectorAll(".plan-priced");
+    if (checked) {
+        prices[0].innerHTML = `$${yearlyPrice[0]}/yr`;
+        prices[1].innerHTML = `$${yearlyPrice[1]}/yr`;
+        prices[2].innerHTML = `$${yearlyPrice[2]}/yr`;
+        setTime(true);
+    }else{
+        prices[0].innerHTML = `$${monthlyPrice[0]}/mo`;
+        prices[1].innerHTML = `$${monthlyPrice[1]}/mo`;
+        prices[2].innerHTML = `$${monthlyPrice[2]}/mo`;
+        setTime(false);
+    }
 }
-firstCircleMobile.onclick = () => {
-    detailsInfo.style.display = "block";
-    detailsSummary.style.display = "none";
-    detailsPlan.style.display = "none";
-    detailsAddon.style.display = "none";
-    backBtn.style.display = "none";
+
+function showAddon(ad, val){
+    const temp = document.getElementsByTagName("template")[0];
+    const clone = temp.content.cloneNode(true);
+    const serviceName = clone.querySelector(".service-name");
+    const servicePrice = clone.querySelector(".service-price");
+    const serviceID = clone.querySelector(".selected-addon");
+    if (ad & val){
+        serviceName.innerText=ad.querySelector("label").innerText;
+        servicePrice.innerText=ad.querySelector(".price").innerText
+        serviceID.setAttribute("data-id", ad.dataset.id);
+        dosument.querySelectorAll(".addons").appendChild(clone);
+    }else{
+        const addons = document.querySelectorAll(".selected-addon");
+        addons.forEach((addon) => {
+            const attr = addon.getAttribute("data-id");
+            if (attr == ad){
+                addon.remove()
+            }
+        })
+    }
 }
-// click function for second circle
-secondCircle.onclick = () =>{
-    detailsInfo.style.display = "none";
-    detailsSummary.style.display = "none";
-    detailsAddon.style.display = "none";
-    detailsPlan.style.display = "block";
-    backBtn.style.display = "block";
+
+function setTotal(){
+    const str = planPrice.innerHTML;
+    const res = str.replace(/\D/g, "");
+    const addonPrices = document.querySelectorAll(".selected-addon .service-price");
+    let val = 0;
+    for (let i = 0; i < addonPrices.length; i++){
+        const str = addonPrices[i].innerHTML;
+        const res = str.replace(/\D/g, ""); 
+        val += Number(res);
+    }
+    total.innerHTML=`$${val + Number(res)}/${time ? "yr" : "no"}`;
 }
-secondCircleMobile.onclick = () =>{
-    detailsInfo.style.display = "none";
-    detailsSummary.style.display = "none";
-    detailsAddon.style.display = "none";
-    detailsPlan.style.display = "block";
-    backBtn.style.display = "block";
-}
-// click function for third circle
-thirdCircle.onclick = () =>{
-    detailsInfo.style.display = "none";
-    detailsSummary.style.display = "none";
-    detailsPlan.style.display = "none";
-    detailsAddon.style.display = "block";
-    backBtn.style.display = "block";
-}
-thirdCircleMobile.onclick = () =>{
-    detailsInfo.style.display = "none";
-    detailsSummary.style.display = "none";
-    detailsPlan.style.display = "none";
-    detailsAddon.style.display = "block";
-    backBtn.style.display = "block";
-}
-// click function for forth circle
-forthCircle.onclick = () =>{
-    detailsInfo.style.display = "none";
-    detailsPlan.style.display = "none";
-    detailsAddon.style.display = "none";
-    detailsSummary.style.display = "block";
-    backBtn.style.display = "block";
-}
-forthCircleMobile.onclick = () =>{
-    detailsInfo.style.display = "none";
-    detailsPlan.style.display = "none";
-    detailsAddon.style.display = "none";
-    detailsSummary.style.display = "block";
-    backBtn.style.display = "block";
+
+function setTime(t){
+    return time = t;
 }
